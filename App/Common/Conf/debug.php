d502e178653b5a25ed3254e0bc79b65ed263838f
rockefy <rockefy@rockefys-MacBook-Air.local>
2015-05-04 15:27:07 +0800

    set debug db config

--- App/Common/Conf/debug.php
+++ App/Common/Conf/debug.php
@@ -12,10 +12,10 @@ return array(
 	
 	//Database
 	'DB_TYPE'   		=> 	'mysql', 	// 数据库类型
-	'DB_HOST'   		=> 	'127.0.0.1',// 服务器地址
+	'DB_HOST'   		=> 	'123.59.54.246',// 服务器地址
 	'DB_NAME'   		=> 	'wms', 	// 数据库名
 	'DB_USER'   		=> 	'root', 	// 用户名
-	'DB_PWD'    		=> 	'dachuwang', 	// 密码
+	'DB_PWD'    		=> 	'42C23744C955C90E30F78CF5053137', 	// 密码
 	'DB_PORT'   		=> 	3306, 		// 端口
 	'DB_PREFIX' 		=> 	'', 		// 数据库表前缀
 
