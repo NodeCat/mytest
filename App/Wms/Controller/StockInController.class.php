98b2a9dbc1c2f31a4d334c474968d4dd427975de
liangdachuwang <liang@dachuwang.com>
2015-06-03 13:56:56 +0800

    还原代码

--- App/Wms/Controller/StockInController.class.php
+++ App/Wms/Controller/StockInController.class.php
@@ -600,9 +600,4 @@ class StockInController extends CommonController {
         if(empty($data))$data['']='';
         echo json_encode($data);
     }
-
-    //结算到货单
-    public function pay(){
-    	
-    }
 }
