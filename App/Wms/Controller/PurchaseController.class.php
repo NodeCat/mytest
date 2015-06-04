3da14396d05884b44609d44dceeadf31ccc57402
王爽 <rockefys@gmail.com>
2015-06-04 17:42:17 +0800

    采购单显示异常

--- App/Wms/Controller/PurchaseController.class.php
+++ App/Wms/Controller/PurchaseController.class.php
@@ -106,8 +106,8 @@ class PurchaseController extends CommonController {
         $this->toolbar_tr =array(
             'view'=>array('name'=>'view', 'show' => isset($this->auth['view']),'new'=>'true'), 
             'edit'=>array('name'=>'edit', 'show' => isset($this->auth['edit']),'new'=>'true','domain'=>"0,11,04,14"), 
-            'pass'=>array('name'=>'pass' ,'show' => isset($this->auth['audit']),'new'=>'true','domain'=>"0,11"),
-            'reject'=>array('name'=>'reject' ,'show' => isset($this->auth['audit']),'new'=>'true','domain'=>"0,11"),
+            'pass'=>array('name'=>'pass' ,'show' => isset($this->auth['pass']),'new'=>'true','domain'=>"0,11"),
+            'reject'=>array('name'=>'reject' ,'show' => isset($this->auth['reject']),'new'=>'true','domain'=>"0,11"),
             'close'=>array('name'=>'close' ,'show' => isset($this->auth['close']),'new'=>'true','domain'=>"0,11,13"),
         );
         
@@ -221,8 +221,6 @@ class PurchaseController extends CommonController {
 			$data['paid_amount'] = $purchase_info['price_total'];
 			M('stock_purchase')->where($map)->save($data);
 		}
-
-
 		$this->msgReturn(1,'','',U('view','id='.$pid));
 	}
 	protected function before_edit() {
@@ -359,16 +357,39 @@ class PurchaseController extends CommonController {
 
 	public function refund() {
 		//通过采购id获取采购单，复制一份改变编号后存到红冲单
-		//通过
-		$M = D(CONTROLLER_NAME);
+		$M = M('purchase');
 		$pk = $M->getPk();
 		$id = I($pk);
 		$map[$M->tableName.'.'.$pk] = $id;
 		$res = $M->where($map)->find();
+
+		//通过采购单号获取到货单id
 		unset($res[$pk]);
 		$res['refer_code'] = $res['code'];
 		$res['code'] = get_sn('out');
-		
+		unset($res['id']);
+		$npid = M('erp_purchase_refund')->add($res);
+
+		//根据到货单id获取到货详情
+		unset($map);
+		$map['refer_code'] = $res['refer_code'];
+		$map['type'] = 1;//采购入库单类型id
+		$map['is_deleted'] = 0;
+		$in = M('stock_bill_in')->field('id')->where($map)->select();
+		$pid = $in[0]['pid'];
+		unset($map);
+
+		//把到货详情拷贝到红冲单详情
+		$map['pid'] = $pid;
+		$detail = M('stock_bill_in_detail')->where($map)->select();
+		$sum = 0;
+		foreach ($detail as $key => &$val) {
+			unset($val['id']);
+			$val['pid'] = $npid;
+			$sum += $val['qualified_qty'] * $val['price_unit'];
+		}
+		$data['for_paid_amount'] = $in['price_total'] - $sum;
+
 		
 	}
 
