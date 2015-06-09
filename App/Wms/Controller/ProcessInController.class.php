8487dd1610aceacb843ba2204858f4b00e3a6bf0
galaxypulse <galaxypulse@163.com>
2015-06-09 10:40:35 +0000

    加工模块测试版

--- App/Wms/Controller/ProcessInController.class.php
+++ App/Wms/Controller/ProcessInController.class.php
@@ -2,7 +2,7 @@
 namespace Wms\Controller;
 use Think\Controller;
 class ProcessInController extends CommonController {
-    //列表显示定义  入库单
+    //列表显示定义
 	protected $columns = array (
 	      'company_id' => '所属系统',
 	      'wh_id' => '所属仓库',
@@ -14,7 +14,7 @@ class ProcessInController extends CommonController {
           'status' => '状态',
 	      'created_time' => '创建时间',
     );
-	//搜索字段定义  入库单
+	//搜索字段定义
 	protected $query   = array (
 	        'erp_process_in.code' => array(
 	                'title' => ' 入库单号',
@@ -52,11 +52,8 @@ class ProcessInController extends CommonController {
 	                'query_type' => 'eq',
 	                'control_type' => 'select',
 	                'value' => array(
-	        	            	'confirm' => '待审核',
-			            'pass' => '已生效',
-			            'reject' => '已驳回',
-			            'close' => '已作废',
-		                'make' => '已生产', 
+	        	            	'parpare' => '待入库',
+			            'on' => '已上架',
 	                ),
 	        ),
 	        'erp_process_in.created_time' => array(
@@ -69,7 +66,7 @@ class ProcessInController extends CommonController {
 	
 	public function before_index() {
 	    $this->table = array(
-	            'toolbar'   => true,
+	            'toolbar'   => false,
 	            'searchbar' => true,
 	            'checkbox'  => true,
 	            'status'    => false,
@@ -79,94 +76,67 @@ class ProcessInController extends CommonController {
 	            array('name'=>'view', 'show' => isset($this->auth['view']),'new'=>'true'),
 	    );
 	}
-	public function _before_index() {
-	    $this->table = array(
-	            'toolbar'   => true,//是否显示表格上方的工具栏,添加、导入等
-	            'searchbar' => true, //是否显示搜索栏
-	            'checkbox'  => true, //是否显示表格中的浮选款
-	            'status'    => false,
-	            'toolbar_tr'=> true,
-	            'statusbar' => true
-	    );
-	    $this->toolbar_tr =array(
-	            'view'=>array('name'=>'view', 'show' => isset($this->auth['view']),'new'=>'true'),
-	            'edit'=>array('name'=>'edit', 'show' => isset($this->auth['edit']),'new'=>'true','domain'=>"draft,confirm"),
-	            'pass'=>array('name'=>'pass' ,'show' => isset($this->auth['pass']),'new'=>'true','domain'=>"draft,confirm"),
-	            'reject'=>array('name'=>'reject' ,'show' => isset($this->auth['reject']),'new'=>'true','domain'=>"draft,confirm"),
-	            'close'=>array('name'=>'close' ,'show' => isset($this->auth['close']),'new'=>'true','domain'=>"draft,confirm,pass,reject")
-	    );
-	    $this->status =array(
-	            array(
-	                    array('name'=>'forbid', 'title'=>'禁用', 'show' => isset($this->auth['forbid'])),
-	                    array('name'=>'resume', 'title'=>'启用', 'show' => isset($this->auth['resume']))
-	            ),
-	    );
-	}
-	
-	
 	
 	/**
-	 * 入库单
 	 * 列表信息处理
 	 * @param unknown $data
 	 */
 	public function after_lists(&$data) {
-	    if (ACTION_NAME == 'index') {
-        	    $in_detail = D('ProcessIn');
-        	    $code = array();
-        	    foreach ($data as &$value) {
-        	        $sql = "select *,company.name from erp_process_in e
-        	                inner join erp_process d on d.code=e.refer_code
-        	                inner join erp_process_sku_relation r on d.p_pro_code=r.p_pro_code
-        	                inner join company on r.company_id=company.id
-        	                where e.code=" . "'".$value['code'] . "' limit 1";
-        	        $result = $in_detail->query($sql);
-        	        $value['company_id'] = $result[0]['name'];
-        	        $value['p_pro_code'] = $result[0]['p_pro_code'];
-        	        
-        	        //格式化仓库
-        	        $warehouse = M('warehouse');
-        	        $map['id'] = $value['wh_id'];
-        	        $result_warehouse = $warehouse->field('name')->where($map)->find();
-        	        $value['wh_id'] = $result_warehouse['name'];
-        	        
-        	        //格式化状态
-        	        switch ($value['status']) {
-        	        	    case 'parpare':
-        	        	        $value['status'] = '待入库';
-        	        	        break;
-        	        	    case 'on':
-        	        	        $value['status'] = '已上架';
-        	        	        break;
-        	        }
-        	        $code[] = $value['p_pro_code'];
-        	    }
-        	    //调用pms接口
-        	    $pms = D('Pms', 'Logic');
-        	    $p_info = $pms->get_SKU_field_by_pro_codes($code);
-        	    if (!empty($p_info)) {
-        	        foreach ($data as &$val) {
-        	            foreach ($p_info as $key => $v) {
-        	                if ($val['p_pro_code'] == $key) {
-        	                    $val['p_pro_name'] = $v['name'];
-        	                    $val['p_pro_norms'] = $v['pro_attrs_str'];
-        	                    break;
-        	                }
-        	            }
-        	        }
-        	    }
+	    $in_detail = D('ProcessIn');
+	    $code = array();
+	    foreach ($data as &$value) {
+	        $sql = "select *,company.name from erp_process_in e
+	                inner join erp_process d on d.code=e.refer_code
+	                inner join erp_process_sku_relation r on d.p_pro_code=r.p_pro_code
+	                inner join company on r.company_id=company.id
+	                where e.code=" . "'".$value['code'] . "' limit 1";
+	        $result = $in_detail->query($sql);
+	        $value['company_id'] = $result[0]['name'];
+	        $value['p_pro_code'] = $result[0]['p_pro_code'];
+	        
+	        //格式化仓库
+	        $warehouse = M('warehouse');
+	        $map['id'] = $value['wh_id'];
+	        $result_warehouse = $warehouse->field('name')->where($map)->find();
+	        $value['wh_id'] = $result_warehouse['name'];
+	        
+	        //格式化状态
+	        switch ($value['status']) {
+	        	    case 'parpare':
+	        	        $value['status'] = '待入库';
+	        	        break;
+	        	    case 'on':
+	        	        $value['status'] = '已上架';
+	        	        break;
+	        }
+	        $code[] = $value['p_pro_code'];
+	    }
+	    $code = array_unique($code);
+	    //调用pms接口
+	    $pms = D('Pms', 'Logic');
+	    $p_info = $pms->get_SKU_field_by_pro_codes($code);
+	    if (!empty($p_info)) {
+	        foreach ($data as &$val) {
+	            foreach ($p_info as $key => $v) {
+	                if ($val['p_pro_code'] == $key) {
+	                    $val['p_pro_name'] = $v['name'];
+	                    $val['p_pro_norms'] = $v['pro_attrs_str'];
+	                    break;
+	                }
+	            }
+	        }
 	    }
 	}
 	
 	/**
-	 * 搜索条件处理  入库单
+	 * 搜索条件处理
 	 * @param unknown $map
 	 */
 	public function after_search(&$map) {
 	   if (array_key_exists('erp_process_in_detail.p_pro_code', $map)) {
 	       //根据父sku编号查询父id
 	       $M = M('erp_process_in_detail');
-	       $where['p_pro_code'] = $map['erp_process_in_detail.p_pro_code'][1];
+	       $where['pro_code'] = $map['erp_process_in_detail.p_pro_code'][1];
 	       $pids = $M->field('pid')->where($where)->select();
 	       
 	       foreach ($pids as $value) {
@@ -188,24 +158,83 @@ class ProcessInController extends CommonController {
 	       }
 	       $p_sku_arr = array_unique($p_sku_arr);
 	       unset($where);
-	       $where['p_pro_code'] = array('in', $p_sku_arr);
-	       $process_ids = $process->field('code')->where($where)->select();
-	       $process_ids_arr = array();
-	       foreach ($process_ids as $v) {
-	           $process_ids_arr[] = $v['code'];
+	       if (!empty($p_sku_arr)) {
+        	       $where['p_pro_code'] = array('in', $p_sku_arr);
+        	       $process_ids = $process->field('code')->where($where)->select();
+        	       $process_ids_arr = array();
+        	       foreach ($process_ids as $v) {
+        	           $process_ids_arr[] = $v['code'];
+        	       }
+        	       if (!array_key_exists($map['erp_process_in.refer_code'])) {
+        	           $map['erp_process_in.refer_code'] = array('in', $process_ids_arr);
+        	       }
+	       } else {
+	           if (!array_key_exists($map['erp_process_in.refer_code'])) {
+	               $map['erp_process_in.refer_code'] = array('eq', null);
+	           }
 	       }
-	       if (!array_key_exists($map['erp_process_in.refer_code'])) {
-	           $map['refer_code'] = array('in', $process_ids_arr);
-	       }
-	       
 	       unset($map['erp_process_sku_relation.company_id']);
+	       
 	   }
 	}
-	
-	/**
-	 * 出库单 
-	 */
-	public function pindex() {
-	    
-	}
+public function before_edit(&$data) {
+        if (empty($data)) {
+            return;
+        }
+        $map['pid'] = $data['id'];
+        $M = M('erp_process_in_detail');
+        $result = $M->where($map)->select();
+        if (!empty($result)) {
+            $data['pros'] = $result;
+            $code = array();
+            $num = 0;
+            foreach ($data['pros'] as $value) {
+                $num += $value['plan_qty'];
+                $code[] = $value['pro_code'];
+            }
+            $code = array_unique($code);
+            
+            //调用pms接口
+            $pms = D('Pms', 'Logic');
+            $p_info = $pms->get_SKU_field_by_pro_codes($code);
+            if (!empty($p_info)) {
+                foreach ($data['pros'] as &$val) {
+                    foreach ($p_info as $key => $v) {
+                        if ($val['pro_code'] == $key) {
+                            $val['pro_name'] = $v['name'];
+                            $val['pro_attrs'] = $v['pro_attrs_str'];
+                            $val['pro_uom'] = '件';
+                            $val['prepare_qty'] = 0; //带上架量
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        unset($map);
+        //格式化仓库
+        $warehouse = M('warehouse');
+        $map['id'] = $data['wh_id'];
+        $result_warehouse = $warehouse->field('name')->where($map)->find();
+        $data['wh_id'] = $result_warehouse['name'];
+         
+        //格式化状态
+        switch ($data['status']) {
+        	case 'parpare':
+        	    $data['status'] = '待出库';
+        	    break;
+        	case 'on':
+        	    $data['status'] = '已出库';
+        	    break;
+        }
+        unset($map);
+        
+        //创建人
+        $map['id'] = $data['created_user'];
+        $user = M('user');
+        $username = $user->field('username')->where($map)->find();
+        $data['created_user'] = $username['username'];
+        $data['total_qty'] = $num;
+        $data['cat_total'] = count($data['pros']);
+    }
 }
