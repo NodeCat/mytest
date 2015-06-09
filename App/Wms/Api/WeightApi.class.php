d0af8954717bac3b33f84f452fecb0c5d32b28ed
王爽 <rockefys@gmail.com>
2015-06-09 12:43:27 +0800

    称重api

--- App/Wms/Api/WeightApi.class.php
+++ App/Wms/Api/WeightApi.class.php
@@ -3,12 +3,12 @@ namespace Wms\Api;
 class WeightApi extends CommApi{
     public function weight() {
         $A = A('Wms/Order','Logic');
-        $res = $A->weight_sku($map);
+        $res = $A->weight_sku(I('post.'));
         $this->ajaxReturn($res);
     }   
     public function lists() {
         $A = A('Wms/Order','Logic');
-        $res = $A->get_details_by_wave_and_sku($map);
+        $res = $A->get_details_by_wave_and_sku(I('post.'));
         $this->ajaxReturn($res);
     } 
 }
