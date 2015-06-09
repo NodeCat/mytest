7fcd3c060f62319fa1c517b4cc9b1bf376568ba9
王爽 <rockefys@gmail.com>
2015-06-09 12:32:16 +0800

    基类api

--- App/Wms/Api/WeightApi.class.php
+++ App/Wms/Api/WeightApi.class.php
@@ -7,7 +7,7 @@ class WeightApi extends CommApi{
         $res = $A->weight_sku($map);
         $this->ajaxReturn($res);
     }   
-    public function list() {
+    public function lists() {
         $A = A('Wms/Order','Logic');
         $res = $A->get_details_by_wave_and_sku($map);
         $this->ajaxReturn($res);
