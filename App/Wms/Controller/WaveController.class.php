82d8683aaa0c512fa1f320d2139e6220ad1c28a1
liangdachuwang <liang@dachuwang.com>
2015-06-15 14:36:59 +0800

    开发

--- App/Wms/Controller/WaveController.class.php
+++ App/Wms/Controller/WaveController.class.php
@@ -5,4 +5,10 @@ class WaveController extends CommonController {
 	protected $columns = array('id' => '',
 			'id' => '',
             );
+
+	//开发测试用
+	public function test(){
+		$wave_ids = array(25,26);
+		A('WavePicking','Logic')->waveExec($wave_ids);
+	}
 }
