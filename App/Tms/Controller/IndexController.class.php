a0f423fa9fb4115cac5e71ef5806b5add209b300
王爽 <rockefys@gmail.com>
2015-06-18 10:14:36 +0800

    fix bug

--- App/Tms/Controller/IndexController.class.php
+++ App/Tms/Controller/IndexController.class.php
@@ -39,7 +39,7 @@ class IndexController extends Controller {
             }
             else {
                 $user = array('mobile'=> $code,'username' => $name);
-                $M1=M('TmsUser');
+                $M1=M('tms_user');
                 $data=$M1->where($user)->find();                 
                 if($data){
                     $date = date('Y-m-d H:i:s',NOW_TIME);
@@ -53,7 +53,7 @@ class IndexController extends Controller {
                     $map['created_time'] = array('between',$start_date.','.$end_date);
                     $map['userid']=$data['id'];
                     unset($M);
-                    $M=M('TmsSignList');
+                    $M=M('tms_sign_list');
                     $id=$M->field('id')->where($map)->find();
                     //如果已经签到过了那就改成最新的签到时间
                     if($id){
