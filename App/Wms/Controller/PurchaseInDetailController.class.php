303ff81166bfa830f9cd6cf3b044cede236c98f2
liangdachuwang <liang@dachuwang.com>
2015-06-26 18:24:18 +0800

    开发完毕

--- App/Wms/Controller/PurchaseInDetailController.class.php
+++ App/Wms/Controller/PurchaseInDetailController.class.php
@@ -8,6 +8,7 @@ class PurchaseInDetailController extends CommonController {
 	protected $columns = array(
         'id' => '',
         'code' => '入库单号',
+        'partner_name' => '供应商',
 		'purchase_code' => '采购单号',
 		'stock_in_code' => '到货单号',
 		'pro_code' => '货品号',
@@ -73,6 +74,21 @@ class PurchaseInDetailController extends CommonController {
         $this->search_addon = true;
     }
 
+    protected function after_search(&$map){
+        if(IS_AJAX){
+            //按照供应商查询
+            $partner_name = I('partner_name');
+            if(!empty($partner_name)){
+                $purchase_codes = $this->getPurchaseCodeMapByPartnerName($partner_name);
+                if($purchase_codes){
+                    $map['purchase_code'] = array('in',$purchase_codes);
+                }else{
+                    $map['purchase_code'] = '';
+                }
+            }
+        }
+    }
+
     //付款
     protected function after_lists(&$data){
         //过滤所有不合格
@@ -133,10 +149,20 @@ class PurchaseInDetailController extends CommonController {
 
     //导出
     public function export(){
-        $purchase_code = I('post.purchase_code');
-        $stock_in_code = I('post.stock_in_code');
-        $pro_code = I('post.pro_code');
-        $status = I('post.status');
+        $purchase_code = I('purchase_code');
+        $stock_in_code = I('stock_in_code');
+        $pro_code = I('pro_code');
+        $status = I('status');
+        //按照供应商查询
+        $partner_name = I('partner');
+        if(!empty($partner_name)){
+            $purchase_codes = $this->getPurchaseCodeMapByPartnerName($partner_name);
+            if($purchase_codes){
+                $map['purchase_code'] = array('in',$purchase_codes);
+            }else{
+                $map['purchase_code'] = '';
+            }
+        }
 
         if(!empty($purchase_code)){
             $map['purchase_code'] = array(array('like','%'.$purchase_code.'%'));
@@ -150,6 +176,7 @@ class PurchaseInDetailController extends CommonController {
         if(!empty($status)){
             $map['status'] = $status;
         }
+
         //查询符合条件的采购入库单
         $purchase_in_details = M('erp_purchase_in_detail')->where($map)->select();
 
@@ -159,6 +186,81 @@ class PurchaseInDetailController extends CommonController {
 
             $this->ajaxReturn($data);
         }
-        var_dump($purchase_in_details);
+
+        $purchase_in_details = A('Pms','Logic')->add_fields($purchase_in_details,'pro_name');
+
+        import("Common.Lib.PHPExcel");
+        import("Common.Lib.PHPExcel.IOFactory");
+        $Excel = new \PHPExcel();
+
+        $Excel->getActiveSheet()->setCellValue('A1', '入库单号'); 
+        $Excel->getActiveSheet()->setCellValue('B1', '采购单号'); 
+        $Excel->getActiveSheet()->setCellValue('C1', '到货单号');
+        $Excel->getActiveSheet()->setCellValue('D1', '货品号');
+        $Excel->getActiveSheet()->setCellValue('E1', '入库数量');
+        $Excel->getActiveSheet()->setCellValue('F1', '单价');
+        $Excel->getActiveSheet()->setCellValue('G1', '小计');
+        $Excel->getActiveSheet()->setCellValue('H1', '支付状态');
+        $Excel->getActiveSheet()->setCellValue('I1', '付款时间');
+
+        $i = 2; 
+        foreach($purchase_in_details as $purchase_in_detail){
+            $Excel->getActiveSheet()->setCellValue('A' . $i, $purchase_in_detail['id']); 
+            $Excel->getActiveSheet()->setCellValue('B' . $i, $purchase_in_detail['purchase_code']); 
+            $Excel->getActiveSheet()->setCellValue('C' . $i, $purchase_in_detail['stock_in_code']);
+            $Excel->getActiveSheet()->setCellValue('D' . $i, $purchase_in_detail['pro_code']); 
+            $Excel->getActiveSheet()->setCellValue('E' . $i, $purchase_in_detail['pro_qty']); 
+            $Excel->getActiveSheet()->setCellValue('F' . $i, $purchase_in_detail['price_unit']); 
+            $Excel->getActiveSheet()->setCellValue('G' . $i, $purchase_in_detail['price_subtotal']); 
+            $Excel->getActiveSheet()->setCellValue('H' . $i, $purchase_in_detail['status']);
+            $Excel->getActiveSheet()->setCellValue('I' . $i, $purchase_in_detail['updated_time']); 
+            $i ++;
+        }
+
+        date_default_timezone_set("Asia/Shanghai");
+        header("Content-Type: application/force-download");
+        header("Content-Type: application/download");
+        header("Content-Transfer-Encoding: binary");
+        header('Accept-Ranges: bytes');
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header("Content-Disposition:attachment;filename = ".time().".xlsx");
+        header('Cache-Control: max-age=0');
+        header("Pragma:no-cache");
+        header("Expires:0");
+        header("Content-Length: ");
+        $objWriter  =  \PHPExcel_IOFactory::createWriter($Excel, 'Excel2007');
+        $objWriter->save('php://output');
+    }
+
+    //根据
+    public function getPurchaseCodeMapByPartnerName($partner_name){
+        //按照供应商查询
+        if(!empty($partner_name)){
+            //根据供应商名称查询对应的id
+            $partner_map['name'] = array('like','%'.$partner_name.'%');
+            $partner_info = M('partner')->where($partner_map)->field('id')->select();
+            unset($partner_map);
+            foreach($partner_info as $partner){
+                $partner_ids[] = $partner['id'];
+            }
+            if(empty($partner_ids)){
+                return false;
+            }
+
+            //根据供应商id 查询对应的采购单号
+            $purchase_map['partner_id'] = array('in',$partner_ids);
+            $purchase_info = M('stock_purchase')->where($purchase_map)->field('code')->select();
+            unset($purchase_map);
+            foreach($purchase_info as $purchase){
+                $purchase_codes[] = $purchase['code'];
+            }
+
+            if(!empty($purchase_codes)){
+                return $purchase_codes;
+            }else{
+                return false;
+            }
+        }
+        return false;
     }
 }
