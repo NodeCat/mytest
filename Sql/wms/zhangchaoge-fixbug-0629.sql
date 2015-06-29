e30e805a36a2127bdf9a7ef01b8d6ade50b7ec35
王爽 <rockefys@gmail.com>
2015-06-29 13:33:22 +0000

    tianjia

---
+++ Sql/wms/zhangchaoge-fixbug-0629.sql
@@ -0,0 +1,2 @@
+UPDATE  `wms`.`numbs` SET  `prefix` =  'DR' WHERE  `numbs`.`name` =  'dis’;
+ALTER TABLE `wms`.`stock_bill_out` ADD dis_mark tinyint(4) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否加入车单 0未加入 1已加入' AFTER delivery_address; 
