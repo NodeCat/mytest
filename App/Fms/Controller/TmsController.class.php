be10ebe2571cd9544a336d843b7b6b1ba2254bb7
王爽 <rockefys@gmail.com>
2015-07-01 21:41:53 +0800

    修改结算金额错误

--- App/Fms/Controller/TmsController.class.php
+++ App/Fms/Controller/TmsController.class.php
@@ -1,11 +1,10 @@
 <?php
 namespace Fms\Controller;
-
 class TmsController extends \Common\Controller\AuthController{
-	public function orders(){
+    public function orders(){
         $id = I('id',0);
         if(!empty($id)) {
-        	$map['id'] = $id;
+            $map['id'] = $id;
             $A = A('Common/Order','Logic');
             $dist = $A->distInfo($map);
             if(empty($dist)) {
@@ -35,18 +34,39 @@ class TmsController extends \Common\Controller\AuthController{
                 };
                 $val['pay_status'] = $s;
                 $val['geo'] = json_decode($val['geo'],TRUE);
-                
                 foreach ($val['detail'] as &$v) {
-                    if($val['status_cn'] == '已签收' || $val['status_cn'] == '已完成' || $val['status_cn'] == '已回款') {
+                    if($val['status_cn'] == '已签收' || $val['status_cn'] == '已完成') {
                         $val['actual_price'] += $v['actual_sum_price'];
                     }
-                    else {
-                        //$val['quantity'] +=$v['quantity'];
+                    elseif($val['status_cn']== '已退货') {
+                        $v['actual_quantity']  = 0;
+                        $v['actual_sum_price'] = 0;
                     }
                 }
+                if($val['actual_price'] > 0) {
+                    $val['pay_for_price'] = $val['actual_price'] - $val['minus_amount'] - $val['pay_reduce'] + $val['deliver_fee'];
+                }
+                else {
+                    $val['pay_for_price'] = 0 ;
+                }
+                
+                if($val['pay_status']=='已付款'){
+                    $val['pay_for_price'] = 0;
+                    $val['deal_price'] = 0;
 
-              	$val['pay_for_price'] = $val['actual_price'] - $val['minus_amount'] - $val['pay_reduce'] + $val['deliver_fee'] + $val['service_fee'];
-                $dist['pay_for_price_total'] += $val['pay_for_price'];
+                }
+                elseif($val['status_cn']== '已退货') {
+                    $val['pay_for_price'] = 0;
+                    $val['deal_price'] = 0;
+                    $val['actual_price'] = 0;
+                }
+                elseif($val['status_cn'] == '已完成') {
+                    $dist['pay_for_price_total'] += $val['deal_price'];
+                }
+                elseif($val['status_cn'] == '已签收') {
+                    $dist['pay_for_price_total'] += $val['pay_for_price'];
+                }
+                
             }
             //dump($orders);
             $this->assign('dist', $dist);
@@ -54,41 +74,56 @@ class TmsController extends \Common\Controller\AuthController{
         }
         $this->display('tms:orders');
     }
-
     public function pay() {
-    	$id = I('get.id',0);
+        $id = I('get.id',0);
         $map['dist_id'] = $id;
         $map['order_by'] = array('user_id'=>'ASC','created_time' => 'DESC');
         $A = A('Common/Order','Logic');
         $orders = $A->order($map);
         foreach ($orders as $val) {
-        	if($val['status_cn'] != '已签收') {
-        		$this->msgReturn('0','结算失败，该配送单含有未处理的订单');
-        	}
+            if($val['status_cn'] != '已签收' && $val['status_cn'] != '已退货' ) {
+                if($val['status_cn'] == '已完成') {
+                    //$this->msgReturn('0','结算失败,该配送单已结算过');
+                }
+                //$this->msgReturn('0','结算失败，该配送单含有未处理的订单');
+            }
         }
         unset($map);
-        $map['status']  = '6';//已结算
-        foreach ($orders as $val) {
-        	foreach ($val['detail'] as &$v) {
-                if($val['status_cn'] == '已签收' || $val['status_cn'] == '已完成' || $val['status_cn'] == '已回款') {
-                    $val['actual_price'] += $v['actual_sum_price'];
+        $map['status']  = '1';//已完成
+        foreach ($orders as &$val) {
+            $val['pay_for_price'] = $val['actual_price'] - $val['minus_amount'] - $val['pay_reduce'] + $val['deliver_fee'];    
+            foreach ($val['detail'] as &$v) {
+                if($val['status_cn'] == '已签收') {
+                    $val['pay_for_price'] += $v['actual_sum_price'];    
+                }
+                elseif($val['status_cn']=='已退货') {
+                    $val['pay_for_price']    = 0;
+                    $row['id']               = $v['id'];
+                    $row['actual_price']     = 0;
+                    $row['actual_quantity']  = 0;
+                    $row['actual_sum_price'] = 0;
+                    $map['order_details'][]  = $row;
+                }
+                if($val['pay_status']=='已付款'){
+                    $val['pay_for_price']=0;
                 }
             }
-
-          	$val['pay_for_price'] = $val['actual_price'] - $val['minus_amount'] - $val['pay_reduce'] + $val['deliver_fee'] + $val['service_fee'];
-            $map['deal_price'] += $val['pay_for_price'];
-
+            $map['deal_price'] = $val['pay_for_price'];
             $order_ids[] = $val['id'];
             $map['suborder_id'] = $val['id'];
+            $map['cur']['name'] = session('user.username');
             $res = $A->set_status($map);
+            unset($map);
         }
+        $this->msgReturn('1','结算成功。');
     }
-    protected function msgReturn($res, $msg='', $data = '', $url=''){
+
+    protected function msgReturn($res, $msg='', $data = '', $url='') {
         $msg = empty($msg)?($res > 0 ?'操作成功':'操作失败'):$msg;
-        if(IS_AJAX){
+        if(IS_AJAX) {
             $this->ajaxReturn(array('status'=>$res,'msg'=>$msg,'data'=>$data,'url'=>$url));
         }
-        else if($res){ 
+        else if($res) { 
                 $this->success($msg,$url);
             }
             else{
@@ -96,5 +131,4 @@ class TmsController extends \Common\Controller\AuthController{
             }
         exit();
     }
-
 }
