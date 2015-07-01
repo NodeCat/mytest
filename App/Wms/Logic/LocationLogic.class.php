a848a843b86bd57445b2caa727d1feb481a6476e
liangdachuwang <liang@dachuwang.com>
2015-07-01 13:53:06 +0800

    波次运行时，添加分配数量，过滤掉收货区 发货区 降级存储区 加工损耗区 加工区 库内报损区 的库存记录

--- App/Wms/Logic/LocationLogic.class.php
+++ App/Wms/Logic/LocationLogic.class.php
@@ -15,13 +15,18 @@ class LocationLogic{
     }
 
     //根据区域标识，查询当前仓库的发货区的location_id
-    public function getPackLocationId($area_name = 'PACK'){
+    public function getPackLocationId($area_name = array()){
         $map['wh_id'] = session('user.wh_id');
-        $map['code'] = $area_name;
-        $location_info = M('Location')->where($map)->field('id')->find();
+        $map['code'] = array('in',$area_name);
+        $location_infos = M('Location')->where($map)->field('id')->select();
         unset($map);
-        $map['pid'] = $location_info['id'];
-        $area_location_info = M('Location')->where($map)->field('id')->find();
+        $location_id = array();
+        foreach($location_infos as $location_info){
+            $location_id[] = $location_info['id'];
+        }
+
+        $map['pid'] = array('in',$location_id);
+        $area_location_info = M('Location')->where($map)->field('id')->select();
 
         return $area_location_info;
     }
