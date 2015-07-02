94a3d880fc8c598cb559056192a5aeca6b6f6611
薛华邦 <xuehuabang@xuehuabangdeMacBook-Air.local>
2015-07-02 17:30:53 +0800

    add baocuo api

---
+++ App/Tms/Api/ReportApi.class.php
@@ -0,0 +1,56 @@
+<?php
+namespace Tms\Api;
+use Think\Controller;
+class ReportApi extends CommApi{
+//根据客户id和报错类型type保存报错信息
+public function report_error(){
+		if($_SERVER['HTTP_CONTENT_TYPE'] == 'application/json'){
+	        $post = json_decode(file_get_contents("php://input"),true);
+	    }else{
+	        $post = I('post.');
+	    }
+
+	    $id = $post['id'];
+	    $type = $post['type'];
+	    $id = 12;
+	    $type = array('定位错误','线路错误');
+	    if(empty($id) || empty($type)){
+	    	unset($data);
+	    	$data = array('status' => '1','msg' => 'error');
+	    	$this->ajaxReturn($data,'JSON');
+	    }else{
+		    $A = A('Common/Order','Logic');
+		    $res = $A->customer(array('id' => $id));	//调用Order逻辑，根据客户id查询客户的信息
+		    if(empty($res)){
+		    	unset($data);
+		    	$data = array('status' => '1','msg' => 'error');
+		    	$this->ajaxReturn($data,'JSON');
+		    }else{
+		    	//保存报错信息到数据库
+			    $M = M('tms_report_error');
+			    $report['type'] = implode(',',$type);
+			    $report['customer_id'] = $id;
+			    $report['customer_name'] = $res['name'];
+			    $report['customer_address'] = $res['address'];
+			    $report['company_id'] = $res['site_id'];
+			    $report['line_id'] = $res['line_id'];
+			    $report['line_name'] = $res['line_name'];
+			    $report['shop_name'] = $res['shop_name'];
+			    $report['current_bd_id'] = $res['sale']['id'];
+			    $report['current_bd'] = $res['sale']['name'];
+			    $report['develop_bd'] = $res['invite_bd'];
+			    $report['driver_name'] = I('session.user');
+			    $report['driver_mobile'] = I('session.mobile');
+			    $report['report_time'] = get_time();
+			    $report['created_time'] = get_time();
+			    $report['created_user'] = UID;
+			    $count = $M->add($report);
+			    if($count){
+			    	unset($data);
+			    	$data = array('status' => '0','msg' => 'OK');
+			    	$this->ajaxReturn($data);
+			    }
+			}
+	    }
+	}
+}
