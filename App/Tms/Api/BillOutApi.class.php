104c9d2e8198ad73f3048386fe75b754f4d7e534
pengyanlei <pengyanlei@pengyanleideMacBook-Air.local>
2015-07-03 11:48:00 +0800

    封装打印小票接口

--- App/Tms/Api/BillOutApi.class.php
+++ App/Tms/Api/BillOutApi.class.php
@@ -30,9 +30,81 @@ class BillOutApi extends CommApi {
         );
         foreach ($data as &$value) {
             $value = is_array($value) ? $this->toStr($value) : $value;
-         }
-         // echo json_encode($data);
-        $this->ajaxReturn($data);
+        }
+        return json_encode($data);
+    }
+
+    /**
+     * [getPrintCommand 获取一条打印指令]
+     * @param  [type] $key [指令对应key]
+     * @return [type]      [description]
+     */
+    public function getPrintCommand($key) {
+        $commands = array(
+            'reset'            => array(0x1B, 0x40),//复位打印机
+            'print'            => array(0x0A),//打印并换行
+            'text_big_size'    => array(0x1B, 0x57, 0x01),//宽高加倍
+            'text_normal_size' => array(0x1B, 0x57, 0x00),//普通字号
+            'no_hightlight'    => array(0x1B, 0x69, 0x00),//禁止反白打印
+        );
+        if(!$key || !isset($commands[$key])){
+            return '';
+        }
+        $command = $this->byteToStr();
+        return $command;
+    }
+
+    /**
+     * [getUnderLine 返回一行横线]
+     */
+    public function getUnderLine() {
+        return '--------------------------------';
+    }
+
+    /**
+     * [formateLine 根据参数，返回一行处理好间隔的字符串用作打印]
+     * @param  string $left   [左边字符]
+     * @param  string $right  [右边字符]
+     * @param  string $center [中间字符]
+     */
+    public function formateLine($left = '', $right = '') {
+        $max  = 32;
+        $llen = mb_strwidth($left, 'utf-8');
+        $rlen = mb_strwidth($left, 'utf-8');
+        $space_len = $max - $llen - $rlen;
+        $sapce = '';
+        for ($i = 0; $i < $space_len; $i++) { 
+            $space .= ' ';
+        }
+        return $left . $space . $right;
+    }
+
+    /**
+     * [formateName 产品名称超过16汉字处理]
+     * @param  string $name [产品名称]
+     * @return [type]       [description]
+     */
+    public function formateName($name = '') {
+        return mb_strimwidth($name, 0, 32, '', 'utf-8');
+    }
+
+    /**
+     * [formatePros 格式化产品数据为可打印字符串]
+     * @param  array  $data [产品数据(二维数组)]
+     * @return [type]       [description]
+     */
+    public function formatePros($data = array()) {
+        $res = array();
+        if($data) {
+            foreach ($data as $pro) {
+                foreach ($value as $val) {
+                    $res[] = $this->formateName($val['proname']);
+                    $res[] = $this->formateLine($val['real_sign_qty'], $val['sum_price']);
+                }
+            }
+        }
+
+        return $res;
     }
 
 	/**
