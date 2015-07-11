cf178333aaeef96bf2438e35ca86247521ddb007
王爽 <rockefys@gmail.com>
2015-07-11 13:19:43 +0000

    xiugai

--- App/Wms/Logic/DistributionLogic.class.php
+++ App/Wms/Logic/DistributionLogic.class.php
@@ -516,7 +516,6 @@ class DistributionLogic {
         foreach ($merge as $key => $v) {
             //用于多种类型出库单库存判断扩展
             $type = $this->get_stock_bill_out_type($key);
-            dump($v);exit();
             if ($type == 'MNO') {
                 //假如是加工出库单 可以这样指定加工出库区库位
                 $idsArr = $stockout_logic->enoughaResult(implode(',', $v), 'WORK-01');
