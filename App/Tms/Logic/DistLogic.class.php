d44d7c921e0e5b497611e98bbeac10d3208ab1d9
pengyanlei <pengyanlei@pengyanleideMacBook-Air.local>
2015-07-14 21:14:15 +0800

    将签收表合并到配送单详情

--- App/Tms/Logic/DistLogic.class.php
+++ App/Tms/Logic/DistLogic.class.php
@@ -5,4 +5,17 @@ class DistLogic {
     protected $wms_api_path = 'Wms/dist/';
     protected $server;
     protected $request;
+
+    public function __construct() {
+        $this->server  = C('HOP_API_PATH');
+        import("Common.Lib.HttpCurl");
+        $this->request = new \HttpCurl();
+    }
+
+    //订单详情--WMS
+    public function distInfo($id) {
+        $action = 'distInfo';
+        $res = R($this->wms_api_path . $action, array($id),'Api');
+        return $res;
+    }
 }
