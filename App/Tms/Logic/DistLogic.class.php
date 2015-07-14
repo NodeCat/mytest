ce79361030b0261be22ffaf8a2d5e3eebb344661
pengyanlei <pengyanlei@pengyanleideMacBook-Air.local>
2015-07-14 21:15:22 +0800

    将签收表合并到配送单详情

--- App/Tms/Logic/DistLogic.class.php
+++ App/Tms/Logic/DistLogic.class.php
@@ -18,4 +18,36 @@ class DistLogic {
         $res = R($this->wms_api_path . $action, array($id),'Api');
         return $res;
     }
+
+    //出库单列表--WMS
+    public function billOut($map = array()){
+        $action = 'lists';
+        //wms API 获取出库单列表
+        $res = R($this->wms_api_path . $action, array($map),'Api');
+        //配送单关联订单信息
+        if($res) {
+            $order_ids = array();
+            foreach ($res as $re) {
+                $order_ids[] = $re['refer_code'];
+            }
+            $map['order_ids'] = $order_ids;
+            $map['itemsPerPage'] = count($res);
+            unset($map['dist_id']);
+            $cA = A('Common/Order','Logic');
+            $orders = $cA->order($map);
+            //配送单关联订单信息
+            foreach ($res as &$bill) {
+                foreach ($orders as $value) {
+                    if($bill['refer_code'] == $value['id']) {
+                        $bill['order_info'] = $value;
+                    }
+                }
+            }
+            $res = array(
+                'orders'     => $res,
+                'orderCount' => count($orders),
+            );
+        }
+        return $res;
+    }
 }
