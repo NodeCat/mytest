2f23d3f446497f1bbabfbaed751f8f1de1dbc3fa
pengyanlei <pengyanlei@pengyanleideMacBook-Air.local>
2015-07-17 17:49:51 +0800

    电子签名调试

--- App/Tms/Controller/IndexController.class.php
+++ App/Tms/Controller/IndexController.class.php
@@ -349,7 +349,7 @@ class IndexController extends Controller {
 
         }
         $this->title = "客户签收";
-        $this->signature_url = C('TMS_API_PATH') . '/signin/signature';
+        $this->signature_url = C('TMS_API_PATH') . '/SignIn/signature';
         $this->display('tms:orders');
     }
 
