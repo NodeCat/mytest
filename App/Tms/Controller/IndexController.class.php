317b60e87c3632ca134e13653c06c1e88ae3a3b6
pengyanlei <pengyanlei@pengyanleideMacBook-Air.local>
2015-07-17 16:49:03 +0800

    电子签名调试

--- App/Tms/Controller/IndexController.class.php
+++ App/Tms/Controller/IndexController.class.php
@@ -690,6 +690,7 @@ class IndexController extends Controller {
 
     //保存客户签名
     public function signature() {
+        dump($_FILES);die();
         $map = array(
             'suborder_id' => I('post.oid/d',0),
             'signature'   => I('post.path','','trim'),
