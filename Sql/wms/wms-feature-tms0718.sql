a2c1b8f05fb3c5fba71742eb576dc80339953b0c
NodeCat <jintam8@163.com>
2015-07-20 17:49:46 +0800

    修改

--- Sql/wms/wms-feature-tms0718.sql
+++ Sql/wms/wms-feature-tms0718.sql
@@ -1,5 +1,2 @@
 ALTER TABLE `wms`.`tms_sign_list` 
-ADD COLUMN `delivery_time` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' AFTER `updated_time`;
-
-ALTER TABLE `wms`.`tms_sign_list` 
 ADD COLUMN `period` CHAR(15) NOT NULL DEFAULT '上午' COMMENT '时段：上午、下午' AFTER `delivery_time`;
