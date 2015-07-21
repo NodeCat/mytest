aa77311db03218afd523cb91a0c2431d7bfa7808
NodeCat <jintam8@163.com>
2015-07-21 20:42:24 +0800

    GPS数据收集

--- Sql/wms/wms-feature-tms0718.sql
+++ Sql/wms/wms-feature-tms0718.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `wms`.`tms_sign_list` 
 ADD COLUMN `period` CHAR(15) NOT NULL DEFAULT '上午' COMMENT '时段：上午、下午' AFTER `delivery_time`;
+
+ALTER TABLE `wms`.`tms_sign_list` 
+ADD COLUMN `delivery_end_time` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '配送结束时间' AFTER `delivery_time`;
