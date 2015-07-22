78473b465a0d04749d7fe16b2f4b958936646310
pengyanlei <pengyanlei@pengyanleideMacBook-Air.local>
2015-07-22 19:09:39 +0800

    签名

--- App/Tms/Api/SignInApi.class.php
+++ App/Tms/Api/SignInApi.class.php
@@ -11,91 +11,67 @@ class SignInApi extends CommApi
     {
         $suborder_id = I('post.order_id/d',0);
         $img = $_FILES['image'];
-        $str = $GLOBALS['HTTP_RAW_POST_DATA'];
-        dump($str);
-        die();
-        if (empty($suborder_id) || empty($img)) {
+        if ($suborder_id == 0 || !$img || $img['error'] != 0) {
             $re = array(
                 'status' => -1,
-                'msg'    => '订单ID或签名不能为空'
+                'msg'    => '参数错误或图片传输失败'
             );
             $this->ajaxReturn($re);
         }
-        dump($img);
-        $path = $this->uploadImg($img);
-        dump($path);die();
-        if ($path) {
-            $map['suborder_id'] = $suborder_id;
-            $map['sign_img'] = $path;
-            //保存签名到配送单详情 和 回调给订单
-            $A = A('Wms/Dist', 'Logic');
-            $cA = A('Common/Order', 'Logic');
-            $ts = $A->saveSignature($map);
-            $hs = $cA->saveSignature($map);
-            if ($ts['status'] === 0 && $hs['status'] === 0) {
-                $re = array(
-                    'status' => 0,
-                    'msg'    => '保存成功'
-                );
-            } else {
-                $re = array(
-                    'status' => -1,
-                    'msg'    => '保存失败，请稍候再试'
-                );
-            }
-        } else {
+
+        $save_dir = './upload';
+        is_dir($save_dir) || mkdir($save_dir, 0755, true);
+        $filename = basename($img['name']);
+        $file = $save_dir . '/' . $filename;
+        if (!move_uploaded_file($img['tmp_name'], $file)) {
+            $re = array(
+                'status' => -1,
+                'msg'    => '图片保存失败'
+            );
+            $this->ajaxReturn($re);
+        }
+        chmod($file,0755);
+        $res = $this->curl_upload_pic($file);
+        if (!$res['url']) {
             $re = array(
                 'status' => -1,
                 'msg'    => '图片上传失败'
             );
+            $this->ajaxReturn($re);
         }
+        $sign_path = $res['saved_path'];
+        $map['suborder_id'] = $suborder_id;
+        $map['sign_img'] = $sign_path;
+        //保存签名到配送单详情 和 回调给订单
+        $A = A('Wms/Dist', 'Logic');
+        $cA = A('Common/Order', 'Logic');
+        $ts = $A->saveSignature($map);
+        $hs = $cA->saveSignature($map);
+        if ($ts['status'] === 0 && $hs['status'] === 0) {
+            $re = array(
+                'status' => 0,
+                'msg'    => '签名保存成功'
+            );
+        } else {
+            $re = array(
+                'status' => -1,
+                'msg'    => '签名保存失败，请稍候再试'
+            );
+        }
+        $this->ajaxReturn($re);
     }
 
-    public function uploadImg($img) {
-        $url = "http://img.dachuwang.com/upload/" + $img['name'];
-        $boundary = '--********--';
-        $header = array(
-            'Content-Type'    => 'multipart/form-data;boundary=' . $boundary,
-            'Charset'         => 'UTF-8',
-            'Connection'      => 'Keep-Alive',
-        );
-        $res = $this->post(
-            $url,
-            $arg, 
-            array('header' => $header)
-        );
-        dump($file);
-        dump($res);
-        return $res;
-    }
-
-    public function post($url, $arg, $options = array(), $urlencode = false, $file = array()){
-        import("Common.Lib.HttpRequest");
-        $httpRequest = new \HttpRequest($url);
-        $httpRequest->set_method("POST");
-        $httpRequest->post_fields = $arg;
-        if ($file) {
-            $httpRequest->add_post_file($file['name'], $file['dir']);
-        }
-        // timeout
-        if(isset($options['timeout'])){
-            $httpRequest->set_timeout($options['timeout']);
-        }else{
-            $httpRequest->set_timeout(8000);
-        }
-        if(isset($options['connect_timeout'])){
-            $httpRequest->set_connect_timeout($options['connect_timeout']);
-        }else{
-            $httpRequest->set_connect_timeout(8000);
-        }
-
-        if(isset($options['header'])){
-            foreach($options['header'] as $key => $value){
-                $httpRequest->add_header($key,$value);
-            }
-        }
-        $httpRequest->send();
-        $response = $httpRequest->get_response_content();
-        return $response;
+   public function curl_upload_pic($file)
+   {
+        $url = "http://img.dachuwang.com/upload?bucket=shop";
+        $fields['files'] = '@'.$file;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url );
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_POST, 1 );
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields );
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($result);
     }
 }
