e0ab137280e8c52b14717fe28159ca52ee895ce1
liangdachuwang <liang@dachuwang.com>
2015-08-05 15:59:33 +0800

    PDA显示当前仓库名称

--- App/Wms/Controller/PdaController.class.php
+++ App/Wms/Controller/PdaController.class.php
@@ -3,10 +3,6 @@ namespace Wms\Controller;
 use Think\Controller;
 class PdaController extends Controller {
 	public function index(){
-		$map['id'] = session('user.wh_id');
-		$warehouse_info = M('warehouse')->where($map)->find();
-
-		$this->warehouse_name = $warehouse_info['name'];
 		C('LAYOUT_NAME','pda');
 		$this->display();
 	}
