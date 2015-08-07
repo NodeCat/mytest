c771c29ee00e5d284ed7a4396d5cd9e50ff96d3e
yanshuaibing <yanshuaibing@dachuwang.com>
2015-08-07 22:46:19 +0800

    加工损耗清单

--- App/Wms/Controller/ProcessLossController.class.php
+++ App/Wms/Controller/ProcessLossController.class.php
@@ -16,12 +16,12 @@ class ProcessLossController extends CommonController
         'p_pro_code' => '父SKU',
         'c_pro_code' => '子SKU',
         'ratio' => '比例',
-        'task' => '原损耗数',
         'c_pro_num' => '原料加工数',
         'p_pro_num' => '成品加工数',
-        'aa' => '损耗率',
-        'over_taskc' => '原料损耗成本',
-        'over_taskd' => '成本损耗成本',
+        'loss_ratio' => '损耗率',
+        'loss_number' => '原料损耗数',
+        'y_loss_amount' => '原料损耗成本',
+        'c_loss_amount' => '成品损耗成本',
     );
     protected $query   = array (
         'erp_process.created_time' => array (
@@ -46,6 +46,8 @@ class ProcessLossController extends CommonController
             'searchbar' => true, //是否显示搜索栏
             'checkbox'  => true, //是否显示表格中的浮选款
         );
+
+        $this->search_addon = true;
     }
 
     /**
@@ -60,50 +62,122 @@ class ProcessLossController extends CommonController
             $value['c_pro_num'] = sprintf('%.2f',$value['p_pro_num']*$value['ratio']);
         }
 
-        $code = implode(',', $pro_code);
+        $start_time = $_POST['query']['erp_process.created_time'];
+        $end_time   = $_POST['query']['erp_process.created_time_1'];
 
-        $model = M('stock');
-        $where['is_deleted'] = 0;
+        if (empty($start_time)) {
+            $start_time = I('get.created_time');
+            $end_time   = I('get.created_time_1');
+        }
 
-        $model->select();
-        console($pro_code);
+        $code   = implode(',', $pro_code);
+        $logic  = D('ProcessLoss', 'Logic');
+        $result = $logic->getStockLoss($code, $start_time, $end_time);
+        foreach ($data as $key => $val) {
+            $data[$key]['loss_number']   = sprintf('%.2f', $result[$val['c_pro_code']]['stock_qty']);
+            $loss_ratio                  = ($data[$key]['loss_number'] / ($val['c_pro_num'] + $data[$key]['loss_number']));       //损耗率
+            $data[$key]['loss_ratio']    = sprintf('%.2f', $loss_ratio * 100).'%';
+            $c_loss_amount               = ($result[$val['c_pro_code']]['total_amount'] / $result[$val['c_pro_code']]['stock_qty']) * $loss_ratio;    //原料损耗成本
+            $data[$key]['c_loss_amount'] = sprintf('%.2f', $c_loss_amount);
+            $data[$key]['y_loss_amount'] = sprintf('%.2f', $data[$key]['c_loss_amount'] * $val['ratio']);   //成品损耗成本
+
+        }
     }
 
-    public function after_search(&$map) {
-        if (array_key_exists('erp_process.p_pro_code', $map)) {
-            $where['p_pro_code'] = $map['erp_process.p_pro_code'][1];
-            $processDetail = M('erp_process_detail')->where($where)->select();
-            if (empty($processDetail)) {
-                unset($map['erp_process.p_pro_code']);
-                return;
-            }
-            $ids = array();
-            foreach ($processDetail as $value) {
-                $ids[] = $value['pid'];
-            }
-            unset($map['erp_process.p_pro_code']);
-            if (!empty($ids)) {
-                $map['erp_process.id'] = array('in', $ids);
-            } else {
-                $map['erp_process.id'] = array('eq', null);
-            }
+    //计算分页条数
+    protected function after_count($param)
+    {
+        $param['count'] = $param['model']->scope('default')->where($param['map'])->group()->count('DISTINCT erp_process_detail.p_pro_code');
+    }
+
+    /**
+     * 进销存导出
+     */
+    public function exportData()
+    {
+        if (!IS_GET) {
+            $this->msgReturn(false, '未知错误');
         }
-        if (array_key_exists('erp_process.created_user', $map)) {
-            $where['nickname'] = $map['erp_process.created_user'][1];
-            $result = M('user')->where($where)->select();
-            if (empty($result)) {
-                unset($map['erp_process.created_user']);
-            }
-            $uids = array();
-            foreach ($result as $value) {
-                $uids[] = $value['id'];
-            }
-            unset($map['erp_process.created_user']);
-            if (!empty($uids)) {
-                $map['erp_process.created_user'] = array('in', $uids);
-            } else {
-                $map['erp_process.created_user'] = array('eq', null);
-            }
+
+        $ids         = I('get.ids');
+        $start_time  = I('get.start_time');
+        $end_time    = I('get.end_time');
+
+        if (empty($start_time) || empty($end_time)){
+            $this->msgReturn(false, '参数错误');
         }
+        $model = D('ProcessLoss');
+
+        if (!empty($ids)) {
+            $where['stock.id']    = array('in', $ids);
+        }
+        $where['DATE_FORMAT(erp_process.`created_time`,\'%Y-%m-%d\')'] = array('between', "$start_time,$end_time");
+        $data = $model->scope('default')->where($where)->select();
+
+        if (!$data) {
+            $this->msgReturn(false, '导出数据为空！');
+        }
+        $pro_code = array();
+        foreach ($data as $key => &$value) {
+            $pro_code[] = $value['c_pro_code'];
+            $value['c_pro_num'] = sprintf('%.2f',$value['p_pro_num']*$value['ratio']);
+        }
+
+        $code   = implode(',', $pro_code);
+        $logic  = D('ProcessLoss', 'Logic');
+        $result = $logic->getStockLoss($code, $start_time, $end_time);
+
+        foreach ($data as $key => $val) {
+            $data[$key]['loss_number']   = sprintf('%.2f', $result[$val['c_pro_code']]['stock_qty']);
+            $data[$key]['y_loss_amount'] = sprintf('%.2f', $result[$val['c_pro_code']]['total_amount']);
+            $data[$key]['c_loss_amount'] = sprintf('%.2f', $result[$val['c_pro_code']]['total_amount'] * $result[$val['c_pro_code']]['stock_qty']);
+            $data[$key]['loss_ratio']    = sprintf('%.2f', $data[$key]['loss_number'] / ($val['c_pro_num'] + $data[$key]['loss_number']) * 100).'%';
+        }
+
+        console($data);
+
+        import("Common.Lib.PHPExcel");
+        import("Common.Lib.PHPExcel.IOFactory");
+        $Excel = new \PHPExcel();
+
+        $sheet = $Excel->createSheet('0');
+        $sheet->setCellValue('A1', '父SKU');
+        $sheet->setCellValue('B1', '子SKU');
+        $sheet->setCellValue('C1', '比例');
+        $sheet->setCellValue('D1', '原料加工数');
+        $sheet->setCellValue('E1', '成品加工数');
+        $sheet->setCellValue('F1', '损耗率');
+        $sheet->setCellValue('G1', '原料损耗数');
+        $sheet->setCellValue('H1', '原料损耗成本');
+        $sheet->setCellValue('I1', '成品损耗成本');
+
+        $i = 1;
+        foreach ($data as $value){
+            $i++;
+            $sheet->setCellValue('A'.$i, $value['p_pro_code']);
+            $sheet->setCellValue('B'.$i, $value['c_pro_code']);
+            $sheet->setCellValue('C'.$i, $value['ratio']);
+            $sheet->setCellValue('D'.$i, $value['c_pro_num']);
+            $sheet->setCellValue('E'.$i, $value['p_pro_num']);
+            $sheet->setCellValue('F'.$i, $value['loss_ratio']);
+            $sheet->setCellValue('G'.$i, $value['loss_number']);
+            $sheet->setCellValue('H'.$i, $value['y_loss_amount']);
+            $sheet->setCellValue('I'.$i, $value['c_loss_amount']);
+        }
+
+        date_default_timezone_set("Asia/Shanghai");
+        header("Content-Type: application/force-download");
+        header("Content-Type: application/download");
+        header("Content-Transfer-Encoding: binary");
+        header('Accept-Ranges: bytes');
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header("Content-Disposition:attachment;filename = ProcessLoss-".date('Y-m-d-H-i-s',time()).".xlsx");
+        header('Cache-Control: max-age=0');
+        header("Pragma:no-cache");
+        header("Expires:0");
+        header("Content-Length: ");
+        $objWriter  =  \PHPExcel_IOFactory::createWriter($Excel, 'Excel2007');
+        $objWriter->save('php://output');
+
     }
 }
--- App/Wms/Model/ProcessLossModel.class.php
+++ App/Wms/Model/ProcessLossModel.class.php
@@ -5,8 +5,6 @@ class ProcessLossModel extends Model {
     protected $readonlyField = array('id');
     public $tableName = 'erp_process';
 
-
-    #SELECT a.code, b.p_pro_code, c.c_pro_code, SUM(b.real_qty) as nums, c.ratio FROM `erp_process` as a INNER JOIN erp_process_detail as b ON b.pid=a.id INNER JOIN erp_process_sku_relation as c ON c.p_pro_code=b.p_pro_code WHERE status=3 GROUP BY b.p_pro_code ORDER BY `a`.`code` ASC
     protected $_scope = array(
         'default'=>array(
             'where'=>array('erp_process.is_deleted'=>'0'),
