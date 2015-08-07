70594b7ce23a53467d7d95b1dc25b76e37b2d36c
薛华邦 <xuehuabang@xuehuabangdeMacBook-Air.local>
2015-08-07 20:40:14 +0800

    派车任务管理系统的计算预计运费功能

--- App/Tms/Logic/DistLogic.class.php
+++ App/Tms/Logic/DistLogic.class.php
@@ -77,23 +77,24 @@ class DistLogic {
         return $price;
     }
     /**
-     * [get_delivery_fee 根据运力平台、车型、公里数获取运费]
-     * @param  [int] $car_platform [运力平台]
+     * [get_delivery_fee 根据车型、公里数获取运费价格最便宜的运力平台及运费]
      * @param  [int] $car_type     [车型]
      * @param  [int] $mile         [公里数]
-     * @return [int] $price        [运费价格]
+     * @return [array] $result        [运力平台及运费价格]
      */
-    public function get_delivery_fee($car_platform = 0, $car_type = 0, $mile = 0) 
+    public function get_delivery_fee($car_type = 0, $mile = 0) 
     {
-        $price = 0;
-        $map['car_platform'] = $car_platform;
+        $result = array();
         $map['car_type']     = $car_type;
         $map['min_mile']     = array('lt',$mile);
         $map['max_mile']     = array('egt',$mile);
         $map['is_deleted']   = 0;
         $M = M('tms_delivery_fee');
-        $res = $M->where($map)->find();
-        $price = $res['price'];
-        return $price;
+        $res = $M->where($map)->order('price asc')->find();
+        if (!empty($res)) {
+            $result['price']        = $res['price'];
+            $result['car_platform'] = $res['car_platform'];
+        }
+        return $result;
     }
 }
