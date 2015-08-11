5362e9e724b200fba4cef2f138ec914a1cbc0c15
liuguangping <liuguangping@dachuwang.com>
2015-08-11 11:08:27 +0800

    提交代码

--- Sql/wms/feature-order.sql
+++ Sql/wms/feature-order.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `stock_bill_out` ADD `customer_id` INT(11) NOT NULL DEFAULT '0' COMMENT '客户id' AFTER `customer_realname`;
+ALTER TABLE `stock_bill_out` ADD `customer_id` INT(11) NULL DEFAULT '0' COMMENT '客户id' AFTER `customer_realname`;
