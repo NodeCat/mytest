e6315bfd036978cc5b1bbc2b88f59422d2dcba13
pengyanlei <pengyanlei@pengyanleideMacBook-Air.local>
2015-08-20 11:37:50 +0800

    签到码当日有效

--- App/Tms/Logic/DistLogic.class.php
+++ App/Tms/Logic/DistLogic.class.php
@@ -245,7 +245,10 @@ class DistLogic {
         $s4 = substr(md5($date . '-' . $wh_id), 0, 4);
         $code = substr(base_convert($s4, 16, 10), -4);
         if (!S(md5($code))) {
-           S(md5($code), $wh_id, 86400); 
+            //保证当日有效
+            $etime = mktime(23, 59, 59, date('m'), date('d'), date('Y'));
+            $expire = $etime - time();
+            S(md5($code), $wh_id, $expire); 
         }
         return $code;
     }
--- Sql/wms/tms-pengyanlei-feature-sign-code.sql
+++ Sql/wms/tms-pengyanlei-feature-sign-code.sql
@@ -39,22 +39,33 @@ VALUES (
 NULL ,  '签到码', NULL ,  'Dispatch/signCode',  '236',  '1',  '1',  '1',  '_self',  'left',  '1',  '0', NULL ,  'Tms'
 );
 
-INSERT INTO  `wms_wangshuang`.`menu` (
+INSERT INTO  `wms_wangshuang`.`auth_authority` (
 `id` ,
 `name` ,
-`icon` ,
-`link` ,
+`type` ,
+`app` ,
+`group` ,
+`module` ,
+`action` ,
+`url` ,
+`condition` ,
+`description` ,
+`title` ,
+`log` ,
+`show` ,
 `pid` ,
+`mpid` ,
 `level` ,
 `queue` ,
-`show` ,
 `target` ,
 `location` ,
 `status` ,
-`is_deleted` ,
-`memo` ,
-`module`
+`updated_user` ,
+`updated_time` ,
+`created_user` ,
+`created_time` ,
+`is_deleted`
 )
 VALUES (
-NULL ,  '签到码', NULL ,  'Dispatch/signCode',  '237',  '2',  '1',  '1',  '_self',  '',  '1',  '0', NULL ,  'Tms'
+NULL ,  'signCode',  '4',  'Erp',  'Tms',  'Dispatch',  'signCode',  'Tms/Dispatch/signCode',  '',  '',  '签到码',  '0',  '1',  '461',  '0',  '0',  '0',  '',  '', '1',  '0',  '0000-00-00 00:00:00',  '0',  '0000-00-00 00:00:00',  '0'
 );
