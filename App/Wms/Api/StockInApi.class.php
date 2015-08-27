0e027642172270377594b561000b09a2447936df
liuguangping <liuguangping@dachuwang.com>
2015-08-27 21:10:29 +0800

    客退入库单关闭

---
+++ App/Wms/Api/StockInApi.class.php
@@ -0,0 +1,85 @@
+<?php
+/**
+* 前端商城订单 进入到wms 转换成出库单
+* @author liang 2015-6-12
+*/
+namespace Wms\Api;
+use Think\Controller;
+class StockInApi extends CommApi{
+
+    //获取入库单状态'0' => '草稿','21'=>'待收货','31'=>'待上架','33'=>'已上架',//'04'=>'已作废'
+    public function getStockInStatus(){
+        //入库单单号
+        $in_code = I('json.in_code');
+        $return = array('status' => 201, 'data' => '', "msg"=>"错误信息");
+        if (!$in_code) {
+            $return = array('status' => 202, 'data' => '', "msg"=>"请合法传参");
+            $this->ajaxReturn($return);
+        }
+        $stock_in_m = M('stock_bill_in');
+        $map = array();
+        $map['code'] = $in_code;
+        $res = $stock_in_m->field('status,is_deleted')->where($map)->find();
+        if ($res) {
+            $result = array();
+            if ($res['is_deleted'] == 1) {
+                $status = "04";
+            } else {
+               $status = $res['status']; 
+            }
+            $result['status'] = $status;
+            $result['in_code'] = $in_code;
+            $return['status'] = 200;
+            $return['data'] = $result;
+            $return['msg'] = "获取状态成功";
+            $this->ajaxReturn($return);
+        } else {
+            $return = array('status' => 203, 'data' => '', "msg"=>"请合法传单，没有该单数据");
+            $this->ajaxReturn($return);
+        }
+    }
+
+    //入库单关闭
+    public function stockInClosed(){
+         //入库单单号
+        $in_code = I('json.in_code');
+        $return = array('status' => 201, 'data' => '', "msg"=>"错误信息");
+        if (!$in_code) {
+            $return = array('status' => 202, 'data' => '', "msg"=>"请合法传参");
+            $this->ajaxReturn($return);
+        }
+        $stock_in_m = M('stock_bill_in');
+        $map = array();
+        $map['code'] = $in_code;
+        $map['is_deleted'] = 0;
+        $result = $stock_in_m->field('id,status')->where($map)->find();
+        if (!$result || !$result['status']) {
+            $return = array('status' => 203, 'data' => '', "msg"=>"请合法传单，没有找到该单数据");
+            $this->ajaxReturn($return);
+        }
+        if ($result['status'] == "21") {
+            unset($map);
+            $map['id']   = $result['id'];
+            $data = array();
+            $data['is_deleted'] = 1;
+            if ($stock_in_m->where($map)->save($data)) {
+                unset($map);
+                $map['pid'] = $result['id'];
+                unset($data);
+                $data['is_deleted'] = 1;
+                M('stock_bill_in_detail')->where($map)->save($data);
+                $return['status'] = 200;
+                $return['data'] = '';
+                $return['msg'] = "关闭成功！";
+                $this->ajaxReturn($return);
+            } else {
+                $return = array('status' => 205, 'data' => '', "msg"=>"该订单关闭失败");
+                $this->ajaxReturn($return); 
+            }
+
+        } else {
+           $return = array('status' => 204, 'data' => '', "msg"=>"该订单状态不能关闭");
+           $this->ajaxReturn($return); 
+        }
+    }
+}
