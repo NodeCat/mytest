fa303ae78cb1a57de2bb437ec89464481d074610
xuehuabang <xuehuabang@126.com>
2015-09-02 13:45:32 +0800

    fms优化

--- Sql/wms/distribution_detail-add-column.sql
+++ Sql/wms/distribution_detail-add-column.sql
@@ -1,3 +1,1 @@
-ALTER TABLE `stock_wave_distribution_detail` ADD `pay_type` TINYINT(2) NOT NULL DEFAULT '0' COMMENT '支付方式:0货到付款,1微信支付,2账期支付' AFTER `deliver_fee`;
-
 ALTER TABLE `stock_wave_distribution_detail` ADD `sign_driver_mobile` varchar(32) NOT NULL DEFAULT '' COMMENT '签收手机号' AFTER `sign_driver`;
