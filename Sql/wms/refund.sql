10e84ab1630476abb95078583bea870297a79bb5
xuehuabang <xuehuabang@126.com>
2015-09-06 16:24:14 +0800

    fms退款单

--- Sql/wms/refund.sql
+++ Sql/wms/refund.sql
@@ -52,3 +52,13 @@ CREATE TABLE IF NOT EXISTS `fms_refund_detail` (
   `is_deleted` tinyint(1) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='退款单详情' AUTO_INCREMENT=1 ;
+
+INSERT INTO `auth_authority` (`id`, `name`, `type`, `app`, `group`, `module`, `action`, `url`, `condition`, `description`, `title`, `log`, `show`, `pid`, `mpid`, `level`, `queue`, `target`, `location`, `status`, `updated_user`, `updated_time`, `created_user`, `created_time`, `is_deleted`) VALUES
+(581, 'index', '3', 'Wms', 'Fms', 'Refund', 'index', 'Fms/Refund/index', '', '', '退款单结算', 0, 1, 380, 0, 0, 0, '', '', '1', 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0),
+(582, 'index', '4', 'Wms', 'Fms', 'Refund', 'index', 'Fms/Refund/index', '', '', '退款单列表', 0, 1, 581, 0, 0, 0, '', '', '1', 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0),
+(583, 'view', '4', 'Wms', 'Fms', 'Refund', 'view', 'Fms/Refund/view', '', '', '退款单详情', 0, 1, 581, 0, 0, 0, '', '', '1', 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 0);
+
+INSERT INTO `menu` (`id`, `name`, `icon`, `link`, `pid`, `level`, `queue`, `show`, `target`, `location`, `status`, `is_deleted`, `memo`, `module`) VALUES
+(241, '退款单结算', NULL, 'Refund/index', 155, 1, 1, 1, '_self', 'left', '1', 0, NULL, 'Fms'),
+(242, '退款单列表', NULL, 'Refund/index', 241, 2, 1, 1, '_self', '', '1', 0, NULL, 'Fms'),
+(243, '退款单详情', NULL, 'Refund/view', 241, 2, 1, 0, '_self', '', '1', 0, NULL, 'Fms');
