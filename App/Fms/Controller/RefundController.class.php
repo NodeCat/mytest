e3769b2d11885c61562db76f795791a039b2feb2
xuehuabang <xuehuabang@126.com>
2015-09-06 17:10:56 +0800

    fms退款单

--- App/Fms/Controller/RefundController.class.php
+++ App/Fms/Controller/RefundController.class.php
@@ -97,7 +97,7 @@ class RefundController extends \Common\Controller\CommonController {
         //$this->search_addon = true;
         $this->toolbar_tr =array(
             array('name'=>'view', 'show' => !isset($auth['view']),'new'=>'true'),
-            array('name'=>'handle','link'=>'handle','class'=>'btn-op should-confirm','icon'=>'ok','title'=>'处理', 'show'=>true,'new'=>'true','domain'=>'0'),
+            array('name'=>'handle','link'=>'handle','class'=>'btn-op should-confirm','icon'=>'ok','title'=>'处理', 'show'=> isset($auth['handle']),'new'=>'true','domain'=>'0'),
         );
 
         $pill = array(
