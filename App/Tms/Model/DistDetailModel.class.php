11b08c9e5387e46e3bc6dc8ad67935f9ed31a578
pengyanlei <pengyanlei@dachuwang.com>
2015-09-07 11:32:34 +0800

    tms数据源优化

---
+++ App/Tms/Model/DistDetailModel.class.php
@@ -0,0 +1,22 @@
+<?php
+namespace Tms\Model;
+use Think\Model\RelationModel;
+
+class DistDetailModel extends RelationModel
+{
+    public $tableName = 'tms_sign_list';
+    protected $_link=array(
+        'TmsUser'=>array(
+        'mapping_type'=>self::BELONGS_TO,
+        'foreign_key'=>'userid',
+        'mapping_fields'=>'username,mobile,car_num,car_type,car_from,warehouse',
+        'as_fields'=>'username,mobile,car_num,car_type,car_from,warehouse',
+        
+        ),
+
+    );
+    
+}
+
+    
+
